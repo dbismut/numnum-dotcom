27172569381dea0b092391ef4ff02c0387c60d38
David Bismut <david.bismut@gmail.com>
2022-03-17 22:33:33 +0100

    add query

--- netlify/functions/get-movies/get-movies.js
+++ netlify/functions/get-movies/get-movies.js
@@ -1,12 +1,15 @@
 import fetch from "node-fetch";
+import qs from "querystring";
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 export const handler = async (event) => {
+  const search = event.queryStringParameters.q || "star wars";
+  console.log(`https://imdb-api.com/en/API/Top250Movies/k_c9j0cngm/${search}`);
   try {
     const response = await fetch(
-      "https://imdb-api.com/en/API/Top250Movies/k_c9j0cngm"
+      `https://imdb-api.com/en/API/Search/k_c9j0cngm/${search}`
     );
     const rawData = await response.json();
-    const items = rawData.items.map((i) => ({
+    const items = rawData.results.map((i) => ({
       id: i.id,
       body: i.title,
       image_url: i.image,
